3dbcdcaeba9edbaa8c207930e8d509efd394bf89
debsmalheiro <malheirodev@gmail.com>
2022-05-23 20:40:08 -0300

    feat: criando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, SetIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    SetIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    SetIsNewTransactionModalOpen(false);
+  };
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
-import logoImg from '../../assets/logo-white.svg';
+import logoImg from "../../assets/logo-white.svg";
 
-import { Container, Content } from './styles';
+import { Container, Content } from "./styles";
 
-export function Header() {
-    return (
-        <Container>
-            <Content>
-                <img src={logoImg} alt="dt money" />
-                <button type="button">
-                    Nova transação
-                </button>
-            </Content>
-        </Container>
-    )
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
 }
