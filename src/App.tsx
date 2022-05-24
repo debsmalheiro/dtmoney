5277d43a89a1bf5b0df3116dfce0569289a54e10
debsmalheiro <malheirodev@gmail.com>
2022-05-24 12:00:22 -0300

    feat: criando componente para o modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import Modal from "react-modal";
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export function App() {
   const [isNewTransactionModalOpen, SetIsNewTransactionModalOpen] =
@@ -22,13 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
